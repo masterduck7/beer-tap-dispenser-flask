96cf83d525b76f24cf280f2db577cd912a804c2f
Luis Ramirez <lp.ramirez.duarte.7@gmail.com>
2023-10-11 21:49:21 +0200

    feat(dispenser): Connect database

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
-from apps.dispenser import dispenser_view
 
+from apps.dispenser import dispenser_view
+from db.database import db
+
+# App setup
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///dispensers.db"
+db.init_app(app)
+
+# Blueprints
 app.register_blueprint(dispenser_view, url_prefix="/dispenser")
 
 
